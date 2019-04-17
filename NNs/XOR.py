19ef5cd8cee68ae9c76b56ef9e9b7f84ccdd22c5
SeongkukCHO <45892679+SeongkukCHO@users.noreply.github.com>
2019-04-17 15:05:00 +0900

    Update XOR.py

--- NNs/XOR.py
+++ NNs/XOR.py
@@ -434,7 +434,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 from scipy.stats import truncnorm
 
-np.random.seed(1) # report submission을 위한 seed값을 활용
+np.random.seed(201904) # report submission을 위한 seed값을 활용
 
 ########################################################################################
 #                              Function                                                #
